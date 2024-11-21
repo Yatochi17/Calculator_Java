469d0511339a33276e0f00f01cb4ed262e0f80f2
Yatochi <aqildanial.17@gmail.com>
2024-11-21 13:49:18 +0800

    add font and declaration

--- src/Calculator.java
+++ src/Calculator.java
@@ -12,6 +12,11 @@ public class Calculator implements ActionListener {
     JButton add_btn, sub_btn, mul_btn, div_btn, dec_btn, equ_btn, del_btn, clr_btn;
     JPanel panel;
 
+    Font myFont = new Font("Montserrat", Font.BOLD, 30);
+
+    double num1 = 0, num2 = 0, result = 0;
+    char operator;
+
     Calculator(){
 
     }
