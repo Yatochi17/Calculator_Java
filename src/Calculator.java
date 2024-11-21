73804c65d303315bf512c6f3996dd31aa0b1fda7
Yatochi <aqildanial.17@gmail.com>
2024-11-21 13:41:14 +0800

    adding instances

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,9 +3,26 @@ import java.awt.*;
 import java.awt.event.*;
 
 
-public class Calculator {
+public class Calculator implements ActionListener {
+
+    JFrame frame;
+    JTextField textField;
+    JButton[] numberButtons  = new JButton[10];
+    JButton[] fxButtons = new JButton[8];
+    JButton add_btn, sub_btn, mul_btn, div_btn, dec_btn, equ_btn, del_btn, clr_btn;
+    JPanel panel;
+
+    Calculator(){
+
+    }
 
     public static void main(String[] args){
+
+        Calculator calc = new Calculator();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
 
     }
 }
