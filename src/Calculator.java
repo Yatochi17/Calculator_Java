41c23e1688af56779d5e828a9a45c55bb1bfe346
Yatochi <aqildanial.17@gmail.com>
2024-11-21 13:41:51 +0800

    Revert "adding instances"
    This reverts commit 73804c65d303315bf512c6f3996dd31aa0b1fda7.

--- src/Calculator.java
+++ src/Calculator.java
@@ -3,26 +3,9 @@ import java.awt.*;
 import java.awt.event.*;
 
 
-public class Calculator implements ActionListener {
-
-    JFrame frame;
-    JTextField textField;
-    JButton[] numberButtons  = new JButton[10];
-    JButton[] fxButtons = new JButton[8];
-    JButton add_btn, sub_btn, mul_btn, div_btn, dec_btn, equ_btn, del_btn, clr_btn;
-    JPanel panel;
-
-    Calculator(){
-
-    }
+public class Calculator {
 
     public static void main(String[] args){
-
-        Calculator calc = new Calculator();
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
 
     }
 }
