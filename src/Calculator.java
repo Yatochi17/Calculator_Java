e0f286fecc9b71cd16d8cd322439d2f5e127fc68
Yatochi <aqildanial.17@gmail.com>
2024-11-21 15:43:37 +0800

    add button fx

--- src/Calculator.java
+++ src/Calculator.java
@@ -8,8 +8,8 @@ public class Calculator implements ActionListener {
     JFrame frame;
     JTextField textField;
     JButton[] numberButtons  = new JButton[10];
-    JButton[] fxButtons = new JButton[8];
-    JButton add_btn, sub_btn, mul_btn, div_btn, dec_btn, equ_btn, del_btn, clr_btn;
+    JButton[] fxButtons = new JButton[9];
+    JButton add_btn, sub_btn, mul_btn, div_btn, dec_btn, equ_btn, del_btn, clr_btn, neg_btn;
     JPanel panel;
 
     Font myFont = new Font("Montserrat", Font.BOLD, 30);
@@ -37,6 +37,7 @@ public class Calculator implements ActionListener {
         equ_btn = new JButton("=");
         del_btn = new JButton("Delete");
         clr_btn = new JButton("Clear");
+        neg_btn = new JButton("(-)");
 
         fxButtons[0] = add_btn;
         fxButtons[1] = sub_btn;
@@ -46,8 +47,9 @@ public class Calculator implements ActionListener {
         fxButtons[5] = equ_btn;
         fxButtons[6] = del_btn;
         fxButtons[7] = clr_btn;
+        fxButtons[8] = neg_btn;
 
-        for(int i = 0; i < 8 ; i++){
+        for(int i = 0; i < 9 ; i++){
             fxButtons[i].addActionListener(this);
             fxButtons[i].setFont(myFont);
             fxButtons[i].setFocusable(false);
@@ -61,8 +63,9 @@ public class Calculator implements ActionListener {
             numberButtons[i].setFocusable(false);
         }
 
-        del_btn.setBounds(50, 430, 145, 50 );
-        clr_btn.setBounds(205, 430, 145, 50);
+        neg_btn.setBounds(50, 430, 100, 50);
+        del_btn.setBounds(150, 430, 100, 50 );
+        clr_btn.setBounds(250, 430, 100, 50);
 
         panel= new JPanel();
         panel.setBounds(50, 100, 300, 300);
@@ -85,8 +88,8 @@ public class Calculator implements ActionListener {
         panel.add(equ_btn);
         panel.add(div_btn);
 
-
         frame.add(panel);
+        frame.add(neg_btn);
         frame.add(del_btn);
         frame.add(clr_btn);
         frame.add(textField);
@@ -105,5 +108,77 @@ public class Calculator implements ActionListener {
     @Override
     public void actionPerformed(ActionEvent e) {
 
+        for (int i = 0; i < 10; i++){
+            if(e.getSource() == numberButtons[i]){
+                textField.setText(textField.getText().concat(String.valueOf(i)));
+            }
+        }
+
+        if (e.getSource() == dec_btn){
+            textField.setText(textField.getText().concat(String.valueOf(".")));
+        }
+
+        if (e.getSource() == add_btn){
+            num1 = Double.parseDouble(textField.getText());
+            operator = '+';
+            textField.setText("");
+        }
+
+        if (e.getSource() == sub_btn){
+            num1 = Double.parseDouble(textField.getText());
+            operator = '-';
+            textField.setText("");
+        }
+
+        if (e.getSource() == mul_btn){
+            num1 = Double.parseDouble(textField.getText());
+            operator = '*';
+            textField.setText("");
+        }
+
+        if (e.getSource() == div_btn){
+            num1 = Double.parseDouble(textField.getText());
+            operator = '/';
+            textField.setText("");
+        }
+
+        if (e.getSource() == equ_btn){
+            num2 = Double.parseDouble(textField.getText());
+
+            switch (operator){
+
+                case '+':
+                    result = num1 + num2;
+                    break;
+
+                case '-':
+                    result = num1 - num2;
+                    break;
+
+                case '*':
+                    result = num1 * num2;
+                    break;
+
+                case '/':
+                    result = num1 / num2;
+                    break;
+            }
+
+            textField.setText(String.valueOf(result));
+            num1 = result;
+
+        }
+
+        if (e.getSource() == clr_btn){
+            textField.setText("");
+        }
+
+        if (e.getSource() == del_btn){
+            String string = textField.getText();
+            textField.setText("");
+            for (int i = 0; i < string.length()-1; i++){
+             textField.setText(textField.getText()+string.charAt(i));
+            }
+        }
     }
 }
